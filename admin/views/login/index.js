9ac81b3579d6c0822b79b6b31be2f9ed67c6ec11
HCVincent <lilacsweet@163.com>
2023-01-11 19:52:46 +0800

    loadview

--- admin/views/login/index.js
+++ admin/views/login/index.js
@@ -1,20 +1,24 @@
-const loginform = document.querySelector("#loginform")
-loginform.onsubmit = async function(evt){
-    loginwarning.style.display = "none"
-    evt.preventDefault()
+const loginform = document.querySelector("#loginform");
+loginform.onsubmit = async function (evt) {
+  loginwarning.style.display = "none";
+  evt.preventDefault();
 
-    console.log(username.value,password.value)
+  console.log(username.value, password.value);
 
-    let res = await fetch(`http://localhost:3000/users?username=${username.value}&password=${password.value}`).then(res=>res.json())
+  let res = await fetch(
+    `http://localhost:3000/users?username=${username.value}&password=${password.value}`
+  ).then((res) => res.json());
 
-    console.log(res[0])
-    if(res.length>0){
-        location.href="/admin/views/home/index.html"
-        
-    }else{
-        //失败
-        console.log("失败")
-        loginwarning.style.display = "block"
-
-    }
-}
+  console.log(res[0]);
+  if (res.length > 0) {
+    localStorage.setItem(
+      "token",
+      JSON.stringify({ ...res[0], password: "******" })
+    );
+    location.href = "/admin/views/home/index.html";
+  } else {
+    //失败
+    console.log("失败");
+    loginwarning.style.display = "block";
+  }
+};
