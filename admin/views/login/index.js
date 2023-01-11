0259f8f8a89bb7392ad2a1a018d5c9c53fdee4e1
HCVincent <lilacsweet@163.com>
2023-01-11 18:04:31 +0800

    f m

---
+++ admin/views/login/index.js
@@ -0,0 +1,20 @@
+const loginform = document.querySelector("#loginform")
+loginform.onsubmit = async function(evt){
+    loginwarning.style.display = "none"
+    evt.preventDefault()
+
+    console.log(username.value,password.value)
+
+    let res = await fetch(`http://localhost:3000/users?username=${username.value}&password=${password.value}`).then(res=>res.json())
+
+    console.log(res[0])
+    if(res.length>0){
+        location.href="/admin/views/home/index.html"
+        
+    }else{
+        //失败
+        console.log("失败")
+        loginwarning.style.display = "block"
+
+    }
+}
